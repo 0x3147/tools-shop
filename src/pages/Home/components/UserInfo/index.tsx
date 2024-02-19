ce0c979c6cebf9278239b974c03dd6741f749126
radeonvii <kjx01771103@cainiao.com>
2024-02-19 11:28:39 +0800

    feat: 登出按钮

--- src/pages/Home/components/UserInfo/index.tsx
+++ src/pages/Home/components/UserInfo/index.tsx
@@ -1,11 +1,16 @@
 import { memo } from 'react'
+import {Button} from 'antd'
 
 import type { FC } from 'react'
 
 const UserInfo: FC = () => {
   return (
-    <main>
+    <main className="flex flex-col">
       <div>UserInfo</div>
+      <div>UserInfo</div>
+      <div className="flex justify-center items-center w-full h-10">
+        <Button className="w-1/2" type="primary" danger>退出登录</Button>
+      </div>
     </main>
   )
 }
